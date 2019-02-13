2eb0c3f7cf3ce90d526a5087988abd2dc7850435
Justin Muskopf <jaymuskopf@gmail.com>
2019-02-13 11:17:18 -0600

    More idiomatic approach

--- lua/autorun/server/sv_restrict_propcore.lua
+++ lua/autorun/server/sv_restrict_propcore.lua
@@ -1,5 +1,7 @@
 -- Propcore is allowed to everyone, but functions in the restrictedFunctions array will be restricted to devotee+ only
 
+wire_expression2_funcs = wire_expression2_funcs or {}
+
 local function restrictPropCoreFunctions()
     local disallowedRanks = {}
     disallowedRanks["user"] = true
@@ -23,21 +25,19 @@ local function restrictPropCoreFunctions()
     }
 
     for _, signature in pairs( restrictedFunctions ) do
-        if wire_expression2_funcs then
-            local oldFunc = wire_expression2_funcs[signature][3]
+        local oldFunc = wire_expression2_funcs[signature][3]
 
-            wire_expression2_funcs[signature][3] = function( self, ... )
-                if ( disallowedRanks[self.player:GetUserGroup()] == nil ) then
-                    local isInBuildMode = self.player:GetNWBool("CFC_PvP_Mode", false) == false
+        wire_expression2_funcs[signature][3] = function( self, ... )
+            if ( disallowedRanks[self.player:GetUserGroup()] == nil ) then
+                local isInBuildMode = self.player:GetNWBool("CFC_PvP_Mode", false) == false
 
-                    if( isInBuildMode or self.player:IsAdmin() ) then
-                        return oldFunc( self, ... )
-                    else
-                        self.player:ChatPrint( "You can't use PropCore in PvP mode" )
-                    end
+                if( isInBuildMode or self.player:IsAdmin() ) then
+                    return oldFunc( self, ... )
                 else
-                    self.player:ChatPrint( "You don't have access to " .. signature )
+                    self.player:ChatPrint( "You can't use PropCore in PvP mode" )
                 end
+            else
+                self.player:ChatPrint( "You don't have access to " .. signature )
             end
         end
     end
