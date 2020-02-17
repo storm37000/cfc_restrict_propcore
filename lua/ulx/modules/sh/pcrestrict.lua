f671044ae15a46269b5a225bc51ee0d9a1afd9c2
Bleck <OverlordBleck@gmail.com>
2020-02-17 14:58:51 -0600

    Remove duplicate, use setOpposite

--- lua/ulx/modules/sh/pcrestrict.lua
+++ lua/ulx/modules/sh/pcrestrict.lua
@@ -1,13 +1,15 @@
 CATEGORY_NAME = "CFC"
 
-local function addToWhitelist( callingPlayer, targetPlayers )
+local function addToWhitelist( callingPlayer, targetPlayers, should_remove )
     local affected_plys = {}
 
     for _, ply in pairs( targetPlayers ) do
         local isPlyWhitelisted = CFCPropcoreRestrict.playerIsWhitelisted( ply )
 
-        if isPlyWhitelisted then
+        if isPlyWhitelisted and not should_remove then
             ULib.tsayError( callingPlayer, ply:Name() .. " is already whitelisted!", true )
+        elseif not isPlyWhitelisted and should_remove then
+            ULib.tsayError( callingPlayer, ply:Name() .. " is not whitelisted!", true )
         else
             table.insert( affected_plys, ply )
         end
@@ -19,27 +21,7 @@ end
 
 local PCWhitelistAdd = ulx.command( CATEGORY_NAME, "ulx allowpropcore", addToWhitelist, "!allowpropcore" )
 PCWhitelistAdd:addParam{ type = ULib.cmds.PlayersArg }
+PCWhitelistAdd:addParam{ type = ULib.cmds.BoolArg, invisible = false }
 PCWhitelistAdd:defaultAccess( ULib.ACCESS_ADMIN )
 PCWhitelistAdd:help( "Adds specified target(s) to a propcore whitelist" )
-
-local function removeFromWhitelist( callingPlayer, targetPlayers )
-    local affected_plys = {}
-
-    for _, ply in pairs( targetPlayers ) do
-        local isPlyWhitelisted = CFCPropcoreRestrict.playerIsWhitelisted( ply )
-
-        if not isPlyWhitelisted then
-            ULib.tsayError( callingPlayer, ply:Name() .. " is not whitelisted!", true )
-        else
-            table.insert( affected_plys, ply )
-        end
-    end
-
-    CFCPropcoreRestrict.removePlayersFromPropcoreWhitelist( affected_plys )
-    ulx.fancyLogAdmin( callingPlayer, true, "#A removed #T from the propcore whitelist", affected_plys )
-end
-
-local PCWhitelistRemove = ulx.command( CATEGORY_NAME, "ulx denypropcore", removeFromWhitelist, "!denypropcore" )
-PCWhitelistRemove:addParam{ type = ULib.cmds.PlayersArg }
-PCWhitelistRemove:defaultAccess( ULib.ACCESS_ADMIN )
-PCWhitelistRemove:help( "Removes specified target(s) from the propcore whitelist" )
+PCWhitelistAdd:setOpposite( "ulx denypropcore", {_, _, true}, "!denypropcore" )
